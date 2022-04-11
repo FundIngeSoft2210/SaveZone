e80f151ac3a727a867874bdc76ed6c228eb618eb
Natalia Gaona Salamanca <70587109+Nat-sourc@users.noreply.github.com>
2022-04-11 18:43:09 -0500

    Se realizaron las navegabilidades y correcciones de pantallas

---
+++ SaveZone/src/main/java/org/example/controllerView/controllerPaginaInicio.java
@@ -0,0 +1,188 @@
+package org.example.controllerView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class controllerPaginaInicio {
+
+    @FXML
+    private Button Boton_Ayuda;
+
+    @FXML
+    private Button Boton_Historial;
+
+    @FXML
+    private Button Boton_VerMisProductos;
+
+    @FXML
+    private ComboBox<?> Boton_categorias;
+
+    @FXML
+    private Button Boton_populares;
+
+    @FXML
+    private Button Boton_vender;
+
+    @FXML
+    private Button Button_uscar;
+
+    @FXML
+    private Button RegresarAlInicio;
+
+    @FXML
+    private ImageView ImagenProducto;
+
+    @FXML
+    private TextField Nombre1;
+
+    @FXML
+    private Button Producto;
+
+    @FXML
+    void Ayuda(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../ConQuePodemosAyudarte.fxml"));
+        Parent root = loader.load();
+        controllerAyuda controllerAyuda = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("../styles.css").toExternalForm());
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../logo.jpg")));
+        stage.setTitle("Ayuda");
+        stage.setScene(scene);
+        stage.setMaximized(false);
+        stage.setResizable(false);
+        stage.show();
+        stage.show();
+
+        Stage myStage = (Stage) this.Boton_Ayuda.getScene().getWindow();
+        myStage.close();
+
+    }
+
+    @FXML
+    void Buscar(ActionEvent event) throws IOException{
+
+    }
+
+    @FXML
+    void BuscarProductos(ActionEvent event) throws IOException{
+
+    }
+
+    @FXML
+    void Categorias(ActionEvent event) throws IOException{
+
+    }
+
+    @FXML
+    void Historial(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Historial.fxml"));
+        Parent root = loader.load();
+        controllerHistorial controllerHistorial = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("../styles.css").toExternalForm());
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../logo.jpg")));
+        stage.setTitle("Historiales");
+        stage.setScene(scene);
+        stage.setMaximized(false);
+        stage.setResizable(false);
+        stage.show();
+        stage.show();
+
+        Stage myStage = (Stage) this.Boton_Historial.getScene().getWindow();
+        myStage.close();
+
+    }
+
+    @FXML
+    void Populares(ActionEvent event) throws IOException{
+
+    }
+
+    @FXML
+    void Productos(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../MisProductos.fxml"));
+        Parent root = loader.load();
+        controllerMisProductos controllerMisProductos = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("../styles.css").toExternalForm());
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../logo.jpg")));
+        stage.setTitle("Ver mis Productos");
+        stage.setScene(scene);
+        stage.setMaximized(false);
+        stage.setResizable(false);
+        stage.show();
+        stage.show();
+
+        Stage myStage = (Stage) this.Boton_VerMisProductos.getScene().getWindow();
+        myStage.close();
+
+    }
+
+    @FXML
+    void VenderProductos(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../anadirProducto.fxml"));
+        Parent root = loader.load();
+        controllerAnadirProducto controllerVender = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("../styles.css").toExternalForm());
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../logo.jpg")));
+        stage.setTitle("VenderTuProducto");
+        stage.setScene(scene);
+        stage.setMaximized(false);
+        stage.setResizable(false);
+        stage.show();
+        stage.show();
+
+        Stage myStage = (Stage) this.Boton_vender.getScene().getWindow();
+        myStage.close();
+
+    }
+
+    @FXML
+    void VerDetallesProducto(ActionEvent event) throws IOException{
+
+    }
+
+    @FXML
+    void VolverInicio(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Principal.fxml"));
+        Parent root = loader.load();
+        controllerPaginaInicio controllerPaginaInicio = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("../styles.css").toExternalForm());
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../logo.jpg")));
+        stage.setTitle("Página Inicio");
+        stage.setScene(scene);
+        stage.setMaximized(false);
+        stage.setResizable(false);
+        stage.show();
+        stage.show();
+
+        Stage myStage = (Stage) this.RegresarAlInicio.getScene().getWindow();
+        myStage.close();
+
+    }
+
+}
+
