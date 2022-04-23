3f2d318e78973172e6525ce39be65922a43411ce
Nicolas Cubillos <nicolasdavidcubillos@gmail.com>
2022-04-22 19:15:51 -0500

    Se agregó el prototipo de la gestión de productos.

--- SaveZone/src/main/java/org/example/Gestion/GestionProductos/GestionPedido.java
+++ SaveZone/src/main/java/org/example/Gestion/GestionProductos/GestionPedido.java
@@ -1,4 +1,24 @@
 package org.example.Gestion.GestionProductos;
 
 public class GestionPedido {
+
+    public boolean crearPedido() {
+        return false;
+    }
+
+    public boolean modificarPedido() {
+        return false;
+    }
+
+    public boolean eliminarPedido() {
+        return false;
+    }
+
+    public boolean rastrearPedido() {
+        return false;
+    }
+
+    public boolean calcularCostoEnvio() {
+        return false;
+    }
 }
